4c7cb9d3c13ec1499e43ff0c7d486aa8ae6180ae
verplancken <42614785+verplancken@users.noreply.github.com>
2023-03-26 21:06:29 -0600

    logica de cursos

--- app/Http/Controllers/HomeUsersController.php
+++ app/Http/Controllers/HomeUsersController.php
@@ -8,9 +8,11 @@ use Illuminate\Http\Request;
 class HomeUsersController extends Controller
 {
     public function index(){
+        $DateAndTime = date('h:i', time());
         $fechaActual = date('Y-m-d');
         $cursos = Cursos::
-        where('fecha_inicial', '>=', $fechaActual)
+        where('fecha_final', '>=', $fechaActual)
+        ->where('hora_final', '>=', $DateAndTime)
         ->orderBy('fecha_inicial','ASC')
         ->get();
 
