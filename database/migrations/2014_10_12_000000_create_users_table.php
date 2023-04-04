61bc377b6cf05c1f41454b3d31b0c127ede1e492
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-04-04 12:06:20 -0600

    Agrengandod datos de migraciones faltanbtes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,9 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('code')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('username')->nullable();
             $table->timestamps();
         });
     }
