5d36e3406d23f069cfcb840f8453a3e10e687ef6
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-04-04 12:45:48 -0600

    Correcion de estilos css en inputs

--- resources/views/user/components/modal_checkout.blade.php
+++ resources/views/user/components/modal_checkout.blade.php
@@ -149,59 +149,53 @@
                   </div>
 
                   <div class="d-flex justify-content-center">
-                    <form
-                            role="form"
-                            action="{{ route('order.pay_stripe') }}"
-                            method="post"
-                            class="require-validation"
-                            data-cc-on-file="false"
-                            data-stripe-publishable-key="{{ env('STRIPE_KEY') }}"
-                            id="payment-form">
+                    <form role="form" action="{{ route('order.pay_stripe') }}" method="post" class="require-validation" data-cc-on-file="false" data-stripe-publishable-key="{{ env('STRIPE_KEY') }}" id="payment-form">
                         @csrf
 
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label'>Name on Card</label> <input
-                                    class='form-control' size='4' type='text'>
-                            </div>
-                        </div>
-
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group card required'>
-                                <label class='control-label'>Card Number</label> <input
-                                    autocomplete='off' class='form-control card-number' size='20'
-                                    type='text'>
-                            </div>
-                        </div>
-
-                        <div class='form-row row'>
-                            <div class='col-xs-12 col-md-4 form-group cvc required'>
-                                <label class='control-label'>CVC</label> <input autocomplete='off'
-                                    class='form-control card-cvc' placeholder='ex. 311' size='4'
-                                    type='text'>
-                            </div>
-                            <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                <label class='control-label'>Expiration Month</label> <input
-                                    class='form-control card-expiry-month' placeholder='MM' size='2'
-                                    type='text'>
-                            </div>
-                            <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                <label class='control-label'>Expiration Year</label> <input
-                                    class='form-control card-expiry-year' placeholder='YYYY' size='4'
-                                    type='text'>
-                            </div>
-                        </div>
-
-                        <div class='form-row row'>
-                            <div class='col-md-12 error form-group hide'>
-                                <div class='alert-danger alert'>Please correct the errors and try
-                                    again.</div>
-                            </div>
-                        </div>
-
                         <div class="row">
-                            <div class="col-xs-12">
-                                <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now ($200)</button>
+                            <div class="col-12">
+                                <div class="input-group flex-nowrap mt-4">
+                                        <span class="input-group-text span_custom_checkout" id=""><i class="fas fa-user"></i></span>
+                                        <input class='form-control' size='4' type='text' placeholder="Nombre del titular">
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="input-group flex-nowrap mt-4">
+                                    <span class="input-group-text span_custom_checkout" id=""><i class="fas fa-credit-card"></i></i></span>
+                                    <input class='form-control card-number' autocomplete='off' size='20' type='text' placeholder="Numero de Tarjeta">
+                                </div>
+                            </div>
+
+                            <div class="col-4">
+                                <div class="input-group flex-nowrap mt-4">
+                                    <span class="input-group-text span_custom_checkout" id=""><i class="fas fa-sort-numeric-up-alt"></i></span>
+                                    <input class='form-control card-cvc' autocomplete='off' placeholder='CVV' size='4' type='text'>
+                                </div>
+                            </div>
+
+                            <div class="col-4">
+                                <div class="input-group flex-nowrap mt-4">
+                                    <span class="input-group-text span_custom_checkout" id=""><i class="fas fa-calendar-alt"></i></i></span>
+                                    <input class='form-control card-expiry-month' autocomplete='off' placeholder='MES' size='2' type='text'>
+                                </div>
+                            </div>
+
+                            <div class="col-4">
+                                <div class="input-group flex-nowrap mt-4">
+                                    <span class="input-group-text span_custom_checkout" id=""><i class="fas fa-sort-alpha-up"></i></span>
+                                    <input class='form-control card-expiry-year' autocomplete='off' placeholder='AÑO' size='4'type='text'>
+                                </div>
+                            </div>
+
+                            <div class="col-12 error form-group hide">
+                                <div class='alert-danger alert'>
+                                    Por favor, corrija los errores e inténtelo de nuevo
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <button class="btn btn-primary btn-lg btn-block" type="submit">Comprar</button>
                             </div>
                         </div>
 
