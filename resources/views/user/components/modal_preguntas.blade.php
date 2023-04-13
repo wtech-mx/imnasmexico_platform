065bd8d5d7bbc74805b025565bc4ca0481ef115e
verplancken <42614785+verplancken@users.noreply.github.com>
2023-04-12 23:29:08 -0600

    Update modal_preguntas.blade.php

--- resources/views/user/components/modal_preguntas.blade.php
+++ resources/views/user/components/modal_preguntas.blade.php
@@ -37,7 +37,6 @@
                                   <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <p class="text_preguntas_material">
-                                            <strong>2. Nuestros cursos y diplomados presenciales están divididos en dos partes:</strong><br>
                                             <strong>Teoría: </strong><br> Se llevará a cabo en salón de clases con las debidas medidas sanitarias. <br> El profesor contará con apoyo visual para brindar la teoría necesaria para tu comprensión. <br><br>
                                             <strong>Práctica: </strong> <br> Se llevará a cabo con camilla personal y auxiliar con <strong>producto incluído.</strong> <br> La práctica se hará de la mano con el profesor en donde se reforzará lo aprendido.
                                         </p>
@@ -54,7 +53,6 @@
                                   <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <p class="text_preguntas_material">
-                                            <strong>3. Online ¿Las clases quedan grabadas permanentemente?</strong><br>
                                             No, después de haber concluido tu clase, tendrás la grabación disponible por <strong>72 horas </strong>para consultarla a la hora que desees.
                                         </p>
                                     </div>
@@ -70,7 +68,6 @@
                                     <div id="flush-collapsefour" class="accordion-collapse collapse" aria-labelledby="flush-four" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <p class="text_preguntas_material">
-                                            <strong>4. ¿Cómo obtengo mi Documento Oficial?</strong><br>
                                             Registro IMNAS o Red CONOCER<br>
                                             1. Te contactará la gestora donde te dará un usuario y contraseña para subir a la plataforma tu información oficial.<br>
                                             2. Una vez aceptada por la gestora IMNAS tendrá máximo un mes para enviarte el documento por paquetería.<br><br>
@@ -91,7 +88,6 @@
                                     <div id="flush-five" class="accordion-collapse collapse" aria-labelledby="flush-five" data-bs-parent="#accordionFlushExample">
                                         <div class="accordion-body">
                                             <p class="text_preguntas_material">
-                                                <strong>5. Presencial ¿Dónde se impartirá mi curso?</strong><br>
                                                 Castilla 136, Álamos, Benito Juárez, 03400 Ciudad de México
                                             </p>
                                         </div>
@@ -107,7 +103,6 @@
                                     <div id="flush-csix" class="accordion-collapse collapse" aria-labelledby="flush-six" data-bs-parent="#accordionFlushExample">
                                         <div class="accordion-body">
                                             <p class="text_preguntas_material">
-                                                <strong>6. Online ¿En qué plataforma se impartirá mi curso?</strong><br>
                                                 Google meet
                                             </p>
                                         </div>
@@ -123,7 +118,6 @@
                                     <div id="flush-collapseseven" class="accordion-collapse collapse" aria-labelledby="flush-seven" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <p class="text_preguntas_material">
-                                            <strong>7. ¿Es necesario tener conocimientos previos?</strong><br>
                                             No se requiere ningún conocimiento ni estudios previos, comenzamos desde 0% y cualquier persona que esté interesada en la materia, lo puede estudiar.<br>
                                         </p>
                                     </div>
@@ -139,7 +133,6 @@
                                     <div id="flush-collapseeigth" class="accordion-collapse collapse" aria-labelledby="flush-eigth" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <p class="text_preguntas_material">
-                                            <strong>8. ¿Tiene un costo extra tramitar mis Documentos Oficiales?</strong><br>
                                             No, al comprar el curso ya te incluye los Documentos Oficiales.<br>
                                         </p>
                                     </div>
@@ -155,7 +148,6 @@
                                     <div id="flush-collapsenine" class="accordion-collapse collapse" aria-labelledby="flush-nine" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <p class="text_preguntas_material">
-                                            <strong>9. Presencial ¿Puedo pagar en efectivo?</strong><br>
                                             Claro, si no te es posible pagar por la plataforma puedes llegar a pagar a la sede.
                                         </p>
                                     </div>
