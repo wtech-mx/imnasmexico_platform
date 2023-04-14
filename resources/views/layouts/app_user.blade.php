654f6b561df98da1523793ad33f390b22a962a18
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-04-14 15:35:49 -0600

    aGREGANDO FAVICON

--- resources/views/layouts/app_user.blade.php
+++ resources/views/layouts/app_user.blade.php
@@ -4,8 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicon/'. $configuracion->favicon) }}"> --}}
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicon/'. $configuracion->favicon) }}">
-    <link rel="icon" type="image/png" href="{{ asset('favicon/'. $configuracion->favicon) }}">
+
+    <link rel="shortcut icon" href="{{asset('assets/user/logotipos/favicon.png')}}" type="image/png">
+
     <title>
         @yield('template_title') - {{$configuracion->nombre_sistema}}
     </title>
