af0c1384544ea4d3652a0b93310077aed5360497
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-05-18 23:21:42 -0600

    Js de carousel items ma coursos

--- resources/views/user/single_coursenew.blade.php
+++ resources/views/user/single_coursenew.blade.php
@@ -653,7 +653,38 @@
 
 
 @endsection
+@section('js')
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+{{-- <script src="{{asset('assets/user/custom/multistepes.js')}}"></script> --}}
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+    integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+    crossorigin="anonymous"></script>
+
+<script>
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        paddimg:30,
+        nav: false,
+        dots:false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+
+        }
+    })
+</script>
+
+@endsection
 
 
 
