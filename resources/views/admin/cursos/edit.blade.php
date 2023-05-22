7196beb31c3522fce3335aed29013b58677e94c3
verplancken <42614785+verplancken@users.noreply.github.com>
2023-05-22 11:49:58 +0200

    Update edit.blade.php

--- resources/views/admin/cursos/edit.blade.php
+++ resources/views/admin/cursos/edit.blade.php
@@ -97,7 +97,7 @@
                                                         <label for="estandar">Seleccionar Profesor</label>
                                                         <select class="form-control" id="id_profesor" name="id_profesor">
 
-                                                            @if ($curso->id_profesor == 0)
+                                                            @if ($curso->id_profesor == 0 || $curso->id_profesor == NULL)
                                                                 <option value="">Seleccione Profesor</option>
                                                             @else
                                                                 <option value="{{ $curso->id_profesor }}">{{ $curso->User->name }}</option>
