c57f57477fe3c1bc093ef5028e531c4277d59762
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-05-29 23:44:08 -0600

    Actiualizacionde iconos del sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -115,7 +115,7 @@
         <li class="nav-item">
             <a data-bs-toggle="collapse" href="#pagesExamplesmarketing" class="nav-link {{ (Request::is('admin/marketing*') ? 'active' : '') }}" aria-controls="pagesExamplesmarketing" role="button" aria-expanded="false">
               <div class="icon icon-shape icon-sm text-center d-flex align-items-center justify-content-center">
-                <i class="fas fa-file-invoice-dollar text-sm opacity-10" style="color: {{$configuracion->color_iconos_sidebar}}"></i>
+                <i class="fas fa-bullhorn text-sm opacity-10" style="color: {{$configuracion->color_iconos_sidebar}}"></i>
               </div>
               <span class="nav-link-text ms-1">Marketing</span>
             </a>
@@ -168,7 +168,7 @@
         <li class="nav-item">
             <a class="nav-link {{ (Request::is('admin/facturas*') ? 'active' : '') }}" href="{{ route('facturas.index') }}">
               <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
-                  <i class="fa fa-users text-sm opacity-10" style="color: {{$configuracion->color_iconos_sidebar}}"></i>
+                  <i class="fas fa-sticky-note text-sm opacity-10" style="color: {{$configuracion->color_iconos_sidebar}}"></i>
               </div>
               <span class="nav-link-text ms-1">Facturas</span>
             </a>
@@ -177,7 +177,7 @@
         <li class="nav-item">
             <a class="nav-link {{ (Request::is('admin/profesores*') ? 'active' : '') }}" href="{{ route('profesores.index') }}">
               <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
-                  <i class="fa fa-users text-sm opacity-10" style="color: {{$configuracion->color_iconos_sidebar}}"></i>
+                  <i class="fa fa-school text-sm opacity-10" style="color: {{$configuracion->color_iconos_sidebar}}"></i>
               </div>
               <span class="nav-link-text ms-1">Profesores</span>
             </a>
