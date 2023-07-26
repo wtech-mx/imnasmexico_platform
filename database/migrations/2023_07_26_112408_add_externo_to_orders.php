6d2bc8163ffa39a35b9b1b7519c198dbb94f5867
verplancken <42614785+verplancken@users.noreply.github.com>
2023-07-26 10:25:41 -0600

    Create 2023_07_26_112408_add_externo_to_orders.php

---
+++ database/migrations/2023_07_26_112408_add_externo_to_orders.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_externo');
+            $table->foreign('id_externo')
+                ->references('id')->on('pagos_fuera')
+                ->inDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
