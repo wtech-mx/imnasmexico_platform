4349e74d92c84470c634d08fead46f6a72d3dc46
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-09-09 11:51:07 -0600

    Agregando la biblioteca al sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -79,6 +79,14 @@
                                 </a>
                             </li>
                         @endcan
+                            <li class="nav-item">
+                                <a class="nav-link {{ (Request::is('/recursos') ? 'active' : '') }}" href="{{ route('recursos.index') }}">
+                                <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
+                                    <i class="fa fa-camera text-sm opacity-10" style="color: {{$configuracion->color_iconos_sidebar}}"></i>
+                                </div>
+                                <span class="nav-link-text ms-1">Biblioteca</span>
+                                </a>
+                            </li>
                         <li class="nav-item">
                             <a data-bs-toggle="collapse" href="#pagesExamplesCursos" class="nav-link {{ (Request::is('admin/notas/cursos*') ? 'active' : '') }}" aria-controls="pagesExamplesCursos" role="button" aria-expanded="false">
                             <div class="icon icon-shape icon-sm text-center d-flex align-items-center justify-content-center">
