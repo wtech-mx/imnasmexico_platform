426daac9715355c86936e2f05d6cb6c6566c38de
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-09-26 14:14:07 -0600

    Acomodadno rutas endentro del middleware

--- routes/web.php
+++ routes/web.php
@@ -355,16 +355,18 @@ Route::group(['prefix' => 'cam', 'middleware' => 'web'], function () {
         return view('cam.auth.login');
     });
 
-    Route::patch('evaluador/videos/edit/{id}', [App\Http\Controllers\Cam\CamClientesController::class, 'update_videos'])->name('evaluador.update_videos');
-
-    // =============== M O D U L O   E X P E D I E N T E S ( C A R P E T A S)===============================
-    Route::get('evaluador/expediente/{code}', [App\Http\Controllers\Cam\CamClientesController::class, 'index_expediente'])->name('evaluador.index_expediente');
-    Route::get('/evaluador/ruta/para/obtener/archivos', [App\Http\Controllers\Cam\CamClientesController::class, 'obtenerArchivosPorCategoria'])->name('evaluador.obtener.archivos');
-    Route::post('/evaluador/expediente/crear/certificado', [App\Http\Controllers\Cam\CamClientesController::class, 'crear_certificado'])->name('evaluador.crear.certificados');
-    Route::post('/evaluador/expediente/crear/cedulas', [App\Http\Controllers\Cam\CamClientesController::class, 'crear_cedulas'])->name('evaluador.crear.cedulas');
-
 
     Route::middleware(['auth'])->group(function () {
+
+
+    Route::patch('evaluador/videos/edit/{id}', [App\Http\Controllers\Cam\CamClientesController::class, 'update_videos'])->name('evaluador.update_videos');
+
+        // =============== M O D U L O   E X P E D I E N T E S ( C A R P E T A S)===============================
+        Route::get('evaluador/expediente/{code}', [App\Http\Controllers\Cam\CamClientesController::class, 'index_expediente'])->name('evaluador.index_expediente');
+        Route::get('/evaluador/ruta/para/obtener/archivos', [App\Http\Controllers\Cam\CamClientesController::class, 'obtenerArchivosPorCategoria'])->name('evaluador.obtener.archivos');
+        Route::post('/evaluador/expediente/crear/certificado', [App\Http\Controllers\Cam\CamClientesController::class, 'crear_certificado'])->name('evaluador.crear.certificados');
+        Route::post('/evaluador/expediente/crear/cedulas', [App\Http\Controllers\Cam\CamClientesController::class, 'crear_cedulas'])->name('evaluador.crear.cedulas');
+
         // =============== M O D U L O   E X P E D I E N T E S  I N D E===============================
         Route::get('/expedientes/independiente', [CamExpedientesController::class, 'index_ind'])->name('independiente.index');
         Route::get('/expediente', [CamExpedientesController::class, 'view'])->name('view.expediente');
