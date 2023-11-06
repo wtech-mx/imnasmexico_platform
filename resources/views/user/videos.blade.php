da0a8cb23a31e6f92f659a014b2f2cf6cd52d2a1
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-11-05 20:48:54 -0600

    Agregnado css

--- resources/views/user/videos.blade.php
+++ resources/views/user/videos.blade.php
@@ -10,7 +10,9 @@
 <link href="{{asset('assets/user/custom/nosotros.css')}}" rel="stylesheet" />
 <link href="{{asset('assets/user/custom/slide_cursos.css')}}" rel="stylesheet" />
 <link href="{{asset('assets/user/custom/slider_products.css')}}" rel="stylesheet" />
-
+{{-- css carrusel --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="crossorigin="anonymous" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" />
 @endsection
 
 @php
