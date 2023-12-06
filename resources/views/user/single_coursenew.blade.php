f294295d5c3a32727c56c2571e06696977bad8ec
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-12-06 14:07:18 -0600

    Mini actualziacion de telefono

--- resources/views/user/single_coursenew.blade.php
+++ resources/views/user/single_coursenew.blade.php
@@ -811,7 +811,7 @@
                         <p class="text_cards_horizon">
                             Si no te es posible realizar el pago por medio de la página,
                             lo podrás hacer mediante estas cuentas enviando tu comprobante al
-                            siguiente número de WhatsApp: 55 4536 5893 con tu nombre completo y nombre del curso y/o diplomado que elegiste.
+                            siguiente número de WhatsApp: <a href="tel:+52 1 55 3116 7046" target="_blank" r>55 3116 7046</a> con tu nombre completo y nombre del curso y/o diplomado que elegiste.
                         </p>
                     </div>
                     <div class="col-12">
