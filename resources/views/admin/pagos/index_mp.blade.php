6864a4a44d874713b3ab6568553dec7f5cb080fd
verplancken <42614785+verplancken@users.noreply.github.com>
2023-12-21 12:25:48 -0600

    oredenes de mp

---
+++ resources/views/admin/pagos/index_mp.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.app_admin')
+
+@section('template_title')
+    Ordenes MP
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+
+                    <div class="card-header">
+
+                        <div class="d-flex justify-content-between">
+
+                            <a class="btn" id="regresar_btn" style="background: {{$configuracion->color_boton_close}}; color: #fff"><i class="fas fa-arrow-left"></i> Regresar </a>
+
+
+                            <h3 class="mb-3">Ordenes MP</h3>
+                            <a type="button" class="btn bg-danger text-white" data-bs-toggle="modal" data-bs-target="#manual_instrucciones">
+                                ¿Como funciona?
+                            </a>
+                        </div>
+                    </div>
+
+                        <div class="card-body">
+
+                            <div class="table-responsive">
+                                <table class="table table-flush" id="datatable-search">
+                                    <thead class="text-center">
+                                        <tr class="tr_checkout">
+                                            <th >Num. Pedido</th>
+                                            <th >Cliente</th>
+                                            <th >Fecha de Compra</th>
+                                            <th >Total</th>
+                                            <th>Forma de Pago</th>
+                                            <th>Estado</th>
+                                            <th>Acciones</th>
+                                        </tr>
+                                        </thead>
+                                    <tbody>
+                                        @foreach ($orders as $order)
+                                            <tr>
+                                                <td>{{ $order->id }}</td>
+                                                <td>{{ $order->User->name }}</td>
+                                                <td>
+                                                    @php
+                                                    $fecha = $order->fecha;
+                                                    $fecha_timestamp = strtotime($fecha);
+                                                    $fecha_formateada = date('d \d\e F \d\e\l Y', $fecha_timestamp);
+                                                    @endphp
+                                                    {{$fecha_formateada}}
+                                                </td>
+                                                <td>{{ $order->pago }}</td>
+                                                <td>{{ $order->forma_pago }}</td>
+                                                <td>
+                                                        Completado
+                                                </td>
+
+                                                <td>
+                                                    <a class="btn btn-sm btn-success" href="{{ route('pagos.edit_pago',$order->id) }}"><i class="fa fa-fw fa-edit"></i> </a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('datatable')
+
+<script>
+    const dataTableSearch = new simpleDatatables.DataTable("#datatable-search", {
+        deferRender:true,
+        paging: true,
+        pageLength: 10
+    });
+</script>
+
+@endsection
