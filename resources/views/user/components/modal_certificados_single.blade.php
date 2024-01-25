a8a07a22fb59e920011d12aa9f69e131b718d867
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-01-25 14:38:34 -0600

    Aggrgando modal

--- resources/views/user/components/modal_certificados_single.blade.php
+++ resources/views/user/components/modal_certificados_single.blade.php
@@ -97,6 +97,33 @@
     </div>
   </div>
 
+        <!-- Diplomado en Cosmetología y Cosmiatría SEP Facial y Corporal -->
+<div class="modal fade" id="dcscufc" tabindex="-1" aria-labelledby="dcscufcLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content" style="border-radius: 19px;border: solid transparent;">
+        <div class="modal-body" style="background: #F5ECE4;border: solid transparent;border-radius: 19px;">
+            <div class="d-flex justify-content-between">
+                <button type="button" class="btn_close_custom" data-bs-dismiss="modal" style="margin-bottom: 1rem;">X</button>
+            </div>
+
+            <p class="text-center">
+                <img src="{{asset('assets/user/certificaciones/alamo1.jpg')}}" alt="" class="img_certificados_general">
+            </p>
+            <p class="text-center">
+                <img src="{{asset('assets/user/certificaciones/alamo2.jpg')}}" alt="" class="img_certificados_general">
+            </p>
+
+            <div class="d-flex justify-content-center">
+                <a class="btn_certificados" href="{{ route('user.avales') }}">
+                    Saber más
+                </a>
+            </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 <!-- Modal revoe-->
 <div class="modal fade" id="revoe" tabindex="-1" aria-labelledby="revoeLabel" aria-hidden="true">
--- resources/views/user/single_coursenew.blade.php
+++ resources/views/user/single_coursenew.blade.php
@@ -626,6 +626,21 @@
                                     </p>
                                 </div>
                                 @endif
+                                @if ($curso->nombre == 'Diplomado en Cosmetología SEP y Cosmiatría UNAM Facial y Corporal')
+                                <div class="col-6 col-md-4 me-0 me-sm-2 me-md-3 me-lg-5">
+                                    <div class="d-flex justify-content-center">
+                                        <div class="card_certificaciones" style="position: relative">
+                                                <a class="text-center" data-bs-toggle="modal" data-bs-target="#dcscufc">
+                                                    <img class="img_card_certificaciones" src="{{asset('assets/user/logotipos/sepconocer.png')}}" alt="">
+                                                </a>
+                                                <img class="click_docmuentos" src="{{asset('assets/user/icons/clic2.png')}}" alt="" >
+                                        </div>
+                                    </div>
+                                    <p class="text-center">
+                                        <?php echo $curso->texto_conocer?>
+                                    </p>
+                                </div>
+                                @endif
                                 @if ($curso->nombre == 'Diplomado Desarrollo Educativo')
                                     <div class="col-6 col-md-4 me-0 me-sm-2 me-md-3 me-lg-5">
                                         <div class="d-flex justify-content-center">
