dcbfa33aef53452f9d8e9515815a072135c2ac18
verplancken <42614785+verplancken@users.noreply.github.com>
2024-02-20 10:02:13 -0600

    ejemplos de llenado

--- resources/views/user/profile_documentos.blade.php
+++ resources/views/user/profile_documentos.blade.php
@@ -9,6 +9,14 @@
                 Descargas/Subir Documentos
             </button>
 
+            @foreach ($estandaresComprados as $estandar)
+                @if ($estandar->nombre == 'EC0010 - Prestación de Servicios Estéticos Corporales SEP CONOCER')
+                    <button class="nav-link" id="nav-des-ejemplo-tab" data-bs-toggle="tab" data-bs-target="#nav-des-ejemplo" type="button" role="tab" aria-controls="nav-des-ejemplo" aria-selected="false">
+                        Ejemplos de llenado
+                    </button>
+                @endif
+            @endforeach
+
             <button class="nav-link" id="nav-estan-doc-tab" data-bs-toggle="tab" data-bs-target="#nav-estan-doc" type="button" role="tab" aria-controls="nav-estan-doc" aria-selected="false">
                 Guia Estandar
             </button>
@@ -381,6 +389,38 @@
                     </p>
                 </div>
             @endif
+        </div>
+    </div>
+
+    <div class="tab-pane fade" id="nav-des-ejemplo" role="tabpanel" aria-labelledby="nav-des-ejemplo-tab" tabindex="0" style="min-height: auto!important;">
+        <div class="modal-body row">
+            <div class="row">
+                <div class="col-6  mb-2">
+                    <a href="{{asset('documentos/FICHA CLINICA CORPORAL.pdf') }}" download="FICHA CLINICA CORPORAL" style="text-decoration: none; color: #000">
+                        <img src="{{asset('assets/user/icons/pdf.png') }}" style="width: 45px; height: 45px;"/>
+                        FICHA CLINICA CORPORAL
+                    </a>
+                </div>
+                <div class="col-6  mb-2">
+                    <iframe class="mt-2" src="{{asset('documentos/FICHA CLINICA CORPORAL.pdf')}}" style="width: 60%; height: 60px;"></iframe>
+                    <p class="text-center ">
+                        <a class="btn btn-sm text-dark" href="{{asset('documentos/FICHA CLINICA CORPORAL.pdf') }}" target="_blank" style="background: #836262; color: #ffff!important">Ver archivo</a>
+                    </p>
+                </div>
+
+                <div class="col-6  mb-2">
+                    <a href="{{asset('documentos/FICHA CLINICA FACIAL.pdf') }}" download="FICHA CLINICA FACIAL" style="text-decoration: none; color: #000">
+                        <img src="{{asset('assets/user/icons/pdf.png') }}" style="width: 45px; height: 45px;"/>
+                        FICHA CLINICA FACIAL
+                    </a>
+                </div>
+                <div class="col-6  mb-2">
+                    <iframe class="mt-2" src="{{asset('documentos/FICHA CLINICA FACIAL.pdf')}}" style="width: 60%; height: 60px;"></iframe>
+                    <p class="text-center ">
+                        <a class="btn btn-sm text-dark" href="{{asset('documentos/FICHA CLINICA FACIAL.pdf') }}" target="_blank" style="background: #836262; color: #ffff!important">Ver archivo</a>
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 
