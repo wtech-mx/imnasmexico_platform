d08db5eb9b8d13ac59e286808f8369828f9f6d31
verplancken <42614785+verplancken@users.noreply.github.com>
2024-03-04 16:04:37 -0600

    botones youtube

--- resources/views/user/profile_documentos.blade.php
+++ resources/views/user/profile_documentos.blade.php
@@ -602,8 +602,8 @@
     <div class="tab-pane fade" id="nav-estan-doc" role="tabpanel" aria-labelledby="nav-estan-doc-tab" tabindex="0" style="min-height: auto!important;">
         <div class="row">
             @foreach ($estandaresComprados as $estandar)
-            <div class="col-12">
-                    <h4 class="text-center">Guía para estándar {{ $estandar->nombre }}</h4> <br>
+                <div class="col-12">
+                        <h4 class="text-center">Guía para evaluar estándar {{ $estandar->nombre }}</h4> <br>
                         @php
                             $documentos_estandar = App\Models\CarpetaDocumentosEstandares::where('id_carpeta', $estandar->id)->where('guia', '=', '1')->get();
                         @endphp
@@ -635,8 +635,119 @@
                                 </div>
                                 @endif
                             @endforeach
+                            @if ($estandar->nombre == 'EC0616 - Prestación de Servicios Auxiliares de Enfermería en Cuidados Básicos y Orientación a Personas en Unidades de Atención Médica SEP CONOCER')
+                                <div class="col-6 mb-5">
+                                    <a href="https://www.youtube.com/watch?v=v06BVRvXvVI" style="text-decoration: none; color: #000" target="_blank">
+                                        <img src="{{asset('assets/user/icons/youtube.png') }}" style="width: 45px; height: 45px;"/>
+                                        Historia de la Enfermería en México
+                                    </a><br>
+                                    <a class="text-center text-white btn btn-sm ml-2" href="https://www.youtube.com/watch?v=v06BVRvXvVI" style="background: #836262; border-radius: 19px;" target="_blank">
+                                        Ver Video
+                                    </a>
+                                </div>
+
+                                <div class="col-6 mb-5">
+                                    <a href="https://www.youtube.com/watch?v=gtAcz6VfkYk" style="text-decoration: none; color: #000" target="_blank">
+                                        <img src="{{asset('assets/user/icons/youtube.png') }}" style="width: 45px; height: 45px;"/>
+                                        ASEO Y CONFORT Uso de chata en hombre
+                                    </a><br>
+                                    <a class="text-center text-white btn btn-sm ml-2" href="https://www.youtube.com/watch?v=gtAcz6VfkYk" style="background: #836262; border-radius: 19px;" target="_blank">
+                                        Ver Video
+                                    </a>
+                                </div>
+
+                                <div class="col-6 mb-5">
+                                    <a href="https://www.youtube.com/watch?v=8K1kOXVJ7wg" style="text-decoration: none; color: #000" target="_blank">
+                                        <img src="{{asset('assets/user/icons/youtube.png') }}" style="width: 45px; height: 45px;"/>
+                                        ASEO Y CONFORT Uso de chata en mujer
+                                    </a><br>
+                                    <a class="text-center text-white btn btn-sm ml-2" href="https://www.youtube.com/watch?v=8K1kOXVJ7wg" style="background: #836262; border-radius: 19px;" target="_blank">
+                                        Ver Video
+                                    </a>
+                                </div>
+
+                                <div class="col-6 mb-5">
+                                    <a href="https://www.youtube.com/watch?v=uoEvPvZ624Q" style="text-decoration: none; color: #000" target="_blank">
+                                        <img src="{{asset('assets/user/icons/youtube.png') }}" style="width: 45px; height: 45px;"/>
+                                        Tendidos de camas
+                                    </a><br>
+                                    <a class="text-center text-white btn btn-sm ml-2" href="https://www.youtube.com/watch?v=uoEvPvZ624Q" style="background: #836262; border-radius: 19px;" target="_blank">
+                                        Ver Video
+                                    </a>
+                                </div>
+
+                                <div class="col-6 mb-5">
+                                    <a href="https://www.youtube.com/watch?v=PGaQjxZp73M" style="text-decoration: none; color: #000" target="_blank">
+                                        <img src="{{asset('assets/user/icons/youtube.png') }}" style="width: 45px; height: 45px;"/>
+                                        Cómo hacer una cama hospitalaria ocupada
+                                    </a><br>
+                                    <a class="text-center text-white btn btn-sm ml-2" href="https://www.youtube.com/watch?v=PGaQjxZp73M" style="background: #836262; border-radius: 19px;" target="_blank">
+                                        Ver Video
+                                    </a>
+                                </div>
+
+                                <div class="col-6 mb-5">
+                                    <a href="https://www.youtube.com/watch?v=chx-bc32vwU" style="text-decoration: none; color: #000" target="_blank">
+                                        <img src="{{asset('assets/user/icons/youtube.png') }}" style="width: 45px; height: 45px;"/>
+                                        La Reanimación Cardiopulmonar (RCP) : Primeros Auxilios
+                                    </a><br>
+                                    <a class="text-center text-white btn btn-sm ml-2" href="https://www.youtube.com/watch?v=chx-bc32vwU" style="background: #836262; border-radius: 19px;" target="_blank">
+                                        Ver Video
+                                    </a>
+                                </div>
+
+                                <div class="col-6 mb-5">
+                                    <a href="https://www.youtube.com/watch?v=JX0dHdFuf5k" style="text-decoration: none; color: #000" target="_blank">
+                                        <img src="{{asset('assets/user/icons/youtube.png') }}" style="width: 45px; height: 45px;"/>
+                                        Oxigenoterapia - Técnica de enfermería
+                                    </a><br>
+                                    <a class="text-center text-white btn btn-sm ml-2" href="https://www.youtube.com/watch?v=JX0dHdFuf5k" style="background: #836262; border-radius: 19px;" target="_blank">
+                                        Ver Video
+                                    </a>
+                                </div>
+
+                                <div class="col-6 mb-5">
+                                    <a href="https://www.youtube.com/watch?v=F1rf1ZLIhZ8" style="text-decoration: none; color: #000" target="_blank">
+                                        <img src="{{asset('assets/user/icons/youtube.png') }}" style="width: 45px; height: 45px;"/>
+                                        TECNICA DE AMORTAJAMIENTO
+                                    </a><br>
+                                    <a class="text-center text-white btn btn-sm ml-2" href="https://www.youtube.com/watch?v=F1rf1ZLIhZ8" style="background: #836262; border-radius: 19px;" target="_blank">
+                                        Ver Video
+                                    </a>
+                                </div>
+
+                                <div class="col-6 mb-5">
+                                    <a href="https://www.youtube.com/watch?v=XPONFUeQeig" style="text-decoration: none; color: #000" target="_blank">
+                                        <img src="{{asset('assets/user/icons/youtube.png') }}" style="width: 45px; height: 45px;"/>
+                                        Enfermeras y turno de noche: cuidados necesarios orientados al descanso del paciente
+                                    </a><br>
+                                    <a class="text-center text-white btn btn-sm ml-2" href="https://www.youtube.com/watch?v=XPONFUeQeig" style="background: #836262; border-radius: 19px;" target="_blank">
+                                        Ver Video
+                                    </a>
+                                </div>
+
+                                <div class="col-6 mb-5">
+                                    <a href="https://www.youtube.com/watch?v=v8G9mxIfEXw" style="text-decoration: none; color: #000" target="_blank">
+                                        <img src="{{asset('assets/user/icons/youtube.png') }}" style="width: 45px; height: 45px;"/>
+                                        Adrenalina (Epinefrina) en Choque anafiláctico By Dr. Zamarrón
+                                    </a><br>
+                                    <a class="text-center text-white btn btn-sm ml-2" href="https://www.youtube.com/watch?v=v8G9mxIfEXw" style="background: #836262; border-radius: 19px;" target="_blank">
+                                        Ver Video
+                                    </a>
+                                </div>
+
+                                <div class="col-6 mb-5">
+                                    <a href="https://www.youtube.com/watch?v=UGhc8mAHrVQ" style="text-decoration: none; color: #000" target="_blank">
+                                        <img src="{{asset('assets/user/icons/youtube.png') }}" style="width: 45px; height: 45px;"/>
+                                        BAÑO DE PACIENTE EN CAMA
+                                    </a><br>
+                                    <a class="text-center text-white btn btn-sm ml-2" href="https://www.youtube.com/watch?v=UGhc8mAHrVQ" style="background: #836262; border-radius: 19px;" target="_blank">
+                                        Ver Video
+                                    </a>
+                                </div>
+                            @endif
                         </div>
-            </div>
+                </div>
             @endforeach
         </div>
     </div>
