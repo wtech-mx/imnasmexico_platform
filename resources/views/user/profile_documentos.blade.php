f1ad64dad20bcbe1f86ed70d0b70d4423c85185b
verplancken <42614785+verplancken@users.noreply.github.com>
2024-06-14 17:45:53 -0600

    Update profile_documentos.blade.php

--- resources/views/user/profile_documentos.blade.php
+++ resources/views/user/profile_documentos.blade.php
@@ -10,7 +10,7 @@
             </button>
 
             @foreach ($estandaresComprados as $estandar)
-                @if ($estandar->nombre == 'EC0010 - Prestación de Servicios Estéticos Corporales SEP CONOCER' || 'EC0186 - Gestión del negocio spa SEP CONOCER')
+                @if ($estandar->nombre == 'EC0010 - Prestación de Servicios Estéticos Corporales SEP CONOCER' || $estandar->nombre == 'EC0186 - Gestión del negocio spa SEP CONOCER')
                     <button class="nav-link" id="nav-des-ejemplo-tab" data-bs-toggle="tab" data-bs-target="#nav-des-ejemplo" type="button" role="tab" aria-controls="nav-des-ejemplo" aria-selected="false">
                         Ejemplos de llenado SEP CONOCER
                     </button>
