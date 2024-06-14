86f15fbf8f88d71f7e0b46f2715af4306dc63e69
verplancken <42614785+verplancken@users.noreply.github.com>
2024-06-14 17:37:09 -0600

    Update tab_ejemplo.blade.php

--- resources/views/user/components/profile/tab_ejemplo.blade.php
+++ resources/views/user/components/profile/tab_ejemplo.blade.php
@@ -28,26 +28,26 @@
             </div>
         @elseif($estandar->nombre == 'EC0186 - Gestión del negocio spa SEP CONOCER')
             <div class="col-6  mb-2">
-                <a href="{{asset('documentos/FICHA CLINICA CORPORAL.pdf') }}" download="FICHA CLINICA CORPORAL" style="text-decoration: none; color: #000">
+                <a href="{{asset('documentos/SPA EJEMPLO 1.pdf') }}" download="SPA EJEMPLO 1" style="text-decoration: none; color: #000">
                     <img src="{{asset('assets/user/icons/pdf.png') }}" style="width: 45px; height: 45px;"/>
                     Ejemplo 1
                 </a>
             </div>
             <div class="col-6  mb-2">
-                <iframe class="mt-2" src="{{asset('documentos/FICHA CLINICA CORPORAL.pdf')}}" style="width: 60%; height: 60px;"></iframe>
+                <iframe class="mt-2" src="{{asset('documentos/SPA EJEMPLO 1.pdf')}}" style="width: 60%; height: 60px;"></iframe>
                 <p class="text-center ">
                     <a class="btn btn-sm text-dark" href="{{asset('documentos/SPA EJEMPLO 1.pdf') }}" target="_blank" style="background: #836262; color: #ffff!important">Ver archivo</a>
                 </p>
             </div>
 
             <div class="col-6  mb-2">
-                <a href="{{asset('documentos/FICHA CLINICA FACIAL.pdf') }}" download="FICHA CLINICA FACIAL" style="text-decoration: none; color: #000">
+                <a href="{{asset('documentos/SPA EJEMPLO 2.pdf') }}" download="SPA EJEMPLO 2" style="text-decoration: none; color: #000">
                     <img src="{{asset('assets/user/icons/pdf.png') }}" style="width: 45px; height: 45px;"/>
                     Ejemplo 2
                 </a>
             </div>
             <div class="col-6  mb-2">
-                <iframe class="mt-2" src="{{asset('documentos/FICHA CLINICA FACIAL.pdf')}}" style="width: 60%; height: 60px;"></iframe>
+                <iframe class="mt-2" src="{{asset('documentos/SPA EJEMPLO 2.pdf')}}" style="width: 60%; height: 60px;"></iframe>
                 <p class="text-center ">
                     <a class="btn btn-sm text-dark" href="{{asset('documentos/SPA EJEMPLO 2.pdf') }}" target="_blank" style="background: #836262; color: #ffff!important">Ver archivo</a>
                 </p>
