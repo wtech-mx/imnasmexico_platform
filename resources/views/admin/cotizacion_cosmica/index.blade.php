50640e0e4afa2ca40efecc9de01718805af0bd06
verplancken <42614785+verplancken@users.noreply.github.com>
2024-08-20 17:16:37 -0600

    se desactivo el ordenamiento

--- resources/views/admin/cotizacion_cosmica/index.blade.php
+++ resources/views/admin/cotizacion_cosmica/index.blade.php
@@ -202,6 +202,8 @@
             responsive: true,
             stateSave: true,
 
+            ordering: false,
+
             language: {
                 url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json'
             }
