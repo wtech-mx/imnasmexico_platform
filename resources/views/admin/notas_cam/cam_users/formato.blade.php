ee1d02ab72e0de2429544708b9eec028f9f73aa0
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-09-04 14:25:12 -0600

    tiitle SOLICITUD DE ACREDITACIÓN INICIAL DE EVALUADOR INDEPENDIENTE

--- resources/views/admin/notas_cam/cam_users/formato.blade.php
+++ resources/views/admin/notas_cam/cam_users/formato.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Contrato</title>
+    <title>SOLICITUD DE ACREDITACIÓN INICIAL DE EVALUADOR INDEPENDIENTE </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
