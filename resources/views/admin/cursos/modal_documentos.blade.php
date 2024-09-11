1d2f8ec6d1ae404048930c6d29267af851cff556
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-09-10 21:52:01 -0600

    Actuzalzaicion de foto

--- resources/views/admin/cursos/modal_documentos.blade.php
+++ resources/views/admin/cursos/modal_documentos.blade.php
@@ -468,29 +468,29 @@
 
                                             @if ($order->User->Documentos)
                                                 @if ($order->User->Documentos->firma !== null)
-                                                <div class="form-group col-6 gc_cn">
-                                                    <label for="name">Firma Personal *</label>
-                                                    <div class="input-group">
-                                                        <span class="input-group-text" id="basic-addon1">
-                                                            <img class="img_profile_label" src="{{asset('assets/user/icons/firma-digital.png')}}" alt="" width="30px">
-                                                        </span>
-                                                        <input id="firma" name="firma" type="file" class="form-control">
-                                                    </div>
-                                                    @if (pathinfo($order->User->Documentos->firma, PATHINFO_EXTENSION) == 'pdf')
-                                                        <a class="text-center text-white btn btn-sm mt-2" href="{{asset('documentos/'. $order->User->telefono . '/' .$order->User->Documentos->firma) }}" download="{{ $order->User->Documentos->firma }}" style="background: #836262; border-radius: 19px;">
-                                                            Descargar Documento
-                                                        </a>
-                                                    @else
+                                                    <div class="form-group col-6 gc_cn">
+                                                        <label for="name">Firma Personal s*</label>
                                                         <div class="input-group">
-                                                            <img id="blah" src="{{asset('documentos/'. $order->User->telefono . '/' .$order->User->Documentos->firma) }}" alt="Imagen" style="width: 100px;height: 100px;">
+                                                            <span class="input-group-text" id="basic-addon1">
+                                                                <img class="img_profile_label" src="{{asset('assets/user/icons/firma-digital.png')}}" alt="" width="30px">
+                                                            </span>
+                                                            <input id="firma" name="firma" type="file" class="form-control">
                                                         </div>
-                                                    @endif
+                                                        @if (pathinfo($order->User->Documentos->firma, PATHINFO_EXTENSION) == 'pdf')
+                                                            <a class="text-center text-white btn btn-sm mt-2" href="{{asset('documentos/'. $order->User->telefono . '/' .$order->User->Documentos->firma) }}" download="{{ $order->User->Documentos->firma }}" style="background: #836262; border-radius: 19px;">
+                                                                Descargar Documento
+                                                            </a>
+                                                        @else
+                                                            <div class="input-group">
+                                                                <img id="blah" src="{{asset('documentos/'. $order->User->telefono . '/' .$order->User->Documentos->firma) }}" alt="Imagen" style="width: 100px;height: 100px;">
+                                                            </div>
+                                                        @endif
 
-                                                </div>
+                                                    </div>
 
                                                 @else
                                                 <div class="form-group col-6 gc_cn">
-                                                    <label for="name">Firma Personal *</label>
+                                                    <label for="name">Firma Personal(*)</label>
                                                     <div class="input-group">
                                                         <span class="input-group-text" id="basic-addon1">
                                                             <img class="img_profile_label" src="{{asset('assets/user/icons/firma-digital.png')}}" alt="" width="30px">
@@ -509,6 +509,29 @@
 
                                                 </div>
                                                 @endif
+
+                                                @if ($order->User->Documentos->foto_infantil_blanco !== null)
+
+                                                <div class="form-group col-6 gc_cn">
+                                                    <label for="name">Fotografia *</label>
+                                                    <div class="input-group">
+                                                        <span class="input-group-text" id="basic-addon1">
+                                                            <img class="img_profile_label" src="{{asset('assets/user/icons/perfil.png')}}" alt="" width="30px">
+                                                        </span>
+                                                        <input id="foto_infantil_blanco" name="foto_infantil_blanco" type="file" class="form-control"  >
+                                                    </div>
+                                                    @if (pathinfo($order->User->Documentos->foto_infantil_blanco, PATHINFO_EXTENSION) == 'pdf')
+                                                        <a class="text-center text-white btn btn-sm mt-2" href="{{asset('documentos/'. $order->User->telefono . '/' .$order->User->Documentos->foto_infantil_blanco) }}" download="{{ $order->User->Documentos->foto_infantil_blanco }}" style="background: #836262; border-radius: 19px;">
+                                                            Descargar Documento
+                                                        </a>
+                                                    @else
+                                                        <img id="blah" src="{{asset('documentos/'. $order->User->telefono . '/' .$order->User->Documentos->foto_infantil_blanco) }}" alt="Imagen" style="width: 100px;height: 100px;">
+                                                    @endif
+                                                </div>
+
+                                            @endif
+
+
                                             @endif
 
                                             @if ($order->User->Documentos)
@@ -529,7 +552,7 @@
                                                         @else
                                                             <img id="blah" src="{{asset('documentos/'. $order->User->telefono . '/' .$order->User->Documentos->foto_tam_infantil) }}" alt="Imagen" style="width: 100px;height: 100px;">
                                                         @endif
-                                                </div>
+                                                    </div>
 
                                                 @endif
                                             @endif
