fcfd7421cd0bb12f32051f50dfc98353baf82396
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-09-24 18:05:50 -0600

    Revert "Actuazlcion de api para recibir datos"
    This reverts commit 721f9b007b369660c8f62c1420e4a3920dfdc51b.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +17,3 @@ use App\Http\Controllers\ApiController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-// En el archivo routes/api.php de Paradisus 2
-
-Route::post('/recibir-notas-pedidos', [ApiController::class, 'recibirNotasPedidos']);
