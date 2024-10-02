3d3b2ed035a229cf6b35ecc77663612a6981baff
verplancken <42614785+verplancken@users.noreply.github.com>
2024-10-02 11:56:54 -0600

    Finalizada

--- resources/views/admin/products/ordenar_cosmica/show.blade.php
+++ resources/views/admin/products/ordenar_cosmica/show.blade.php
@@ -109,7 +109,7 @@ Productos solicitados Cosmica
                                                 </div>
                                             @endif
 
-                                            @if ($pedido->estatus != 'Finalizado')
+                                            @if ($pedido->estatus != 'Finalizada')
                                                 <div class="form-group col-2">
                                                     <label>Cantidad recibida</label>
                                                     <div class="input-group mb-3">
@@ -140,7 +140,7 @@ Productos solicitados Cosmica
                                 </div>
 
                             </div>
-                            @if ($pedido->estatus != 'Finalizado')
+                            @if ($pedido->estatus != 'Finalizada')
                                 <div class="modal-footer">
                                     <button type="submit" class="btn close-modal" style="background: {{$configuracion->color_boton_save}}; color: #ffff">Guardar</button>
                                 </div>
