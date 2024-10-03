d8ddf30ed60496eba9851cd29f84b1f276bbb1e4
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-10-03 11:03:55 -0600

    Quitnado workshop

--- resources/views/user/components/navbar_cosmika.blade.php
+++ resources/views/user/components/navbar_cosmika.blade.php
@@ -96,10 +96,10 @@
           <li class="nav-item">
             <a class="nav-link nav_link_custom "  href="https://cosmicaskin.com/about/ ">¿Quienes Somos?</a>
           </li>
-
+{{-- 
           <li class="nav-item">
             <a class="nav-link nav_link_custom" target="" href="https://cosmicaskin.com/workshop/">Workshop</a>
-          </li>
+          </li> --}}
 
           <a class="navbar-brand" href="{{ route('user.home') }}">
             <img src="{{asset('assets/user/logotipos/cosmica.png')}}" class="image_cosmika d-inline-block align-text-top" style="">
