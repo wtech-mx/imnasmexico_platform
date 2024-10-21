de56523238b01c0b82511dbf6d0c14a4611b4815
verplancken <42614785+verplancken@users.noreply.github.com>
2024-10-21 13:01:39 -0600

    acetntps

--- resources/views/admin/registro_imnas/plantilla_buscador_general.blade.php
+++ resources/views/admin/registro_imnas/plantilla_buscador_general.blade.php
@@ -126,8 +126,8 @@
                     <div class="col-12 col-md-6 col-lg-6 my-auto">
                         <a href="{{ route('folio.index_cedula',$tickets_generador->folio) }}" class="btn btn-xs mt-3 w-100 text-dark" style="background-color: #DDD78D">Cédula  de Identidad de Papel</a>
                         <a href="{{ route('folio.index_crednecial',$tickets_generador->folio) }}" class="btn btn-xs mt-3 w-100 text-dark" style="background-color: #DCBF85">Credencial Plastificada</a>
-                        <a href="{{ route('folio.index_diploma',$tickets_generador->folio) }}" class="btn btn-xs mt-3 w-100 text-white" style="background-color: #8B635C">Diploma Imnnas</a>
-                        <a href="{{ route('folio.index_titulo',$tickets_generador->folio) }}" class="btn btn-xs mt-3 w-100 text-white" style="background-color: #60594D">Diploma de Profesionalización</a>
+                        <a href="{{ route('folio.index_diploma',$tickets_generador->folio) }}" class="btn btn-xs mt-3 w-100 text-white" style="background-color: #8B635C">Diploma de Profesionalización</a>
+                        <a href="{{ route('folio.index_titulo',$tickets_generador->folio) }}" class="btn btn-xs mt-3 w-100 text-white" style="background-color: #60594D">Titulo Honorífico</a>
                         <a href="{{ route('folio.index_tira',$tickets_generador->folio) }}" class="btn btn-xs mt-3 w-100 text-white" style="background-color: #93A29B">Tira de Materias</a>
                     </div>
 
@@ -199,8 +199,8 @@
             <div class="col-12 col-md-6 col-lg-6 my-auto">
                 <a href="{{ route('folio.index_cedula',$tickets_externo->folio) }}" class="btn btn-xs mt-3 w-100 text-dark" style="background-color: #DDD78D">Cédula  de Identidad de Papel</a>
                 <a href="{{ route('folio.index_crednecial',$tickets_externo->folio) }}" class="btn btn-xs mt-3 w-100 text-dark" style="background-color: #DCBF85">Credencial Plastificada</a>
-                <a href="{{ route('folio.index_diploma',$tickets_externo->folio) }}" class="btn btn-xs mt-3 w-100 text-white" style="background-color: #8B635C">Diploma Imnnas</a>
-                <a href="{{ route('folio.index_titulo',$tickets_externo->folio) }}" class="btn btn-xs mt-3 w-100 text-white" style="background-color: #60594D">Diploma de Profesionalización</a>
+                <a href="{{ route('folio.index_diploma',$tickets_externo->folio) }}" class="btn btn-xs mt-3 w-100 text-white" style="background-color: #8B635C">Diploma de Profesionalización</a>
+                <a href="{{ route('folio.index_titulo',$tickets_externo->folio) }}" class="btn btn-xs mt-3 w-100 text-white" style="background-color: #60594D">Titulo Honorífico</a>
             </div>
 
         </div>
