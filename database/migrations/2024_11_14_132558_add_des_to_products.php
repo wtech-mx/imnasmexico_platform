3d59095b11606cb6cb5a35778fad999c27ba6ce0
verplancken <42614785+verplancken@users.noreply.github.com>
2024-11-14 14:28:20 -0600

    db

---
+++ database/migrations/2024_11_14_132558_add_des_to_products.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->text('envase_lab')->nullable();
+            $table->text('conteo_lab')->nullable();
+            $table->text('descripcion_lab')->nullable();
+            $table->text('caja_lab')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
---
+++ database/migrations/2024_11_14_133028_add_des_to_historial_stock.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('historial_stock', function (Blueprint $table) {
+            $table->text('tipo_cambio')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('historial_stock', function (Blueprint $table) {
+            //
+        });
+    }
+};
