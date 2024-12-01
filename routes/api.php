b7d8a67f1684f9418e85d987b764d78e60f38665
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-11-30 20:55:23 -0600

    Envio de Prodcutos Bundle

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Products;
+use App\Models\ProductosBundleId;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,3 +32,17 @@ Route::get('/enviar-productos', function () {
         'data' => $products
     ]);
 });
+
+Route::get('/enviar-productos/bundle-pack', function () {
+    // Definir los estatus que deseas filtrar
+
+    $productsBundle = ProductosBundleId::get();
+
+    // Retornar los datos en formato JSON
+    return response()->json([
+        'success' => true,
+        'message' => 'Datos enviados correctamente',
+        'data' => $productsBundle
+    ]);
+});
+
