48218cbe29bfc6fe3fd4622dc5cdd5664aa47028
verplancken <42614785+verplancken@users.noreply.github.com>
2025-01-14 17:25:39 -0600

    url qr

--- resources/views/admin/bodega/index_preparados.blade.php
+++ resources/views/admin/bodega/index_preparados.blade.php
@@ -109,12 +109,6 @@
                                                     <i class="fa fa-list-alt"></i>
                                                 </a>
 
-                                                <a class="text-center text-white btn btn-sm"
-                                                    href="{{ route('pdf_etiqueta.bodega', ['tabla' => 'notas_productos', 'id' => $item->id]) }}"
-                                                    style="background: #7d2de6;">
-                                                    <i class="fa fa-qrcode"></i>
-                                                </a>
-
                                                 <a type="button" class="btn btn-sm btn-danger text-white" data-bs-toggle="modal" data-bs-target="#estatusFechasModal{{$item->id}}">
                                                     <i class="fa fa-info"></i>
                                                 </a>
@@ -228,7 +222,7 @@
                                                 </a>
 
                                                 <a class="text-center text-white btn btn-sm"
-                                                    href="{{ route('pdf_etiqueta.bodega', ['tabla' => 'notas_productos', 'id' => $item->id]) }}"
+                                                    href="{{ route('pdf_etiqueta.bodega', ['tabla' => 'notas_cosmica_productos', 'id' => $item->id]) }}"
                                                     style="background: #7d2de6;">
                                                     <i class="fa fa-qrcode"></i>
                                                 </a>
