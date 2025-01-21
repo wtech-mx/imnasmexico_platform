57ef73e6299fb6b39e4b3e825762eb150bf34d28
verplancken <42614785+verplancken@users.noreply.github.com>
2025-01-20 23:02:27 -0600

    cambio de cards a componente

--- resources/views/tienda_cosmica/productos.blade.php
+++ resources/views/tienda_cosmica/productos.blade.php
@@ -50,48 +50,8 @@
                 <div class="tab-pane fade show active" id="pills-todos" role="tabpanel" aria-labelledby="pills-todos-tab" tabindex="0">
                     <div class="row">
 
-                        @foreach ( $products as $item)
-
-                            @php
-                                $nombre = $item->nombre;
-                                $partes = explode(' ', $nombre, 2); // Divide el nombre en máximo 2 partes
-                                $primeraPalabra = $partes[0]; // La primera palabra
-                                $restoDelTexto = $partes[1] ?? ''; // El resto del texto, si existe
-                            @endphp
-
-                            <div class="col-6 col-sm-6 col-md-4 col-lg-3">
-                                <div class="container_lineas_single_carousel mx-auto" style="width:80%; ">
-                                    <div class="content mb-3 mt-3">
-                                        <div class="img_container_single_corousel mx-auto">
-                                            <img class="img_grid_products_single_carousel" src="{{$item->imagenes}}" alt="Protector">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <h2 class="text-center titulo_producto_carousel mt-4">
-                                    <strong>{{ $primeraPalabra }}</strong> <br>
-                                    {{ $restoDelTexto }}
-                                </h2>
-
-                                <h4 class="text-center price_producto_carousel mt-3">
-                                    <strong class="">${{ $item->precio_normal }}.00</strong>
-                                </h4>
-
-                                <p class="text-center">
-
-                                    <a href="" class="text-center mt-3">
-                                        <img class="icon_single_item_slide" src="{{ asset('cosmika/inicio/CORAZON-POPULARES.png') }}" alt="" style="width: 25px;margin-right: auto;margin-left: auto;">
-                                    </a>
-                                </p>
-                                <p class="text-center">
-
-                                    <a href="" class="text-center mt-3">
-                                        <img class="icon_single_item_slide" src="{{ asset('cosmika/inicio/AGREGAR-POPULARES.png') }}" alt="" style="width: 25px;margin-right: auto;margin-left: auto;">
-                                    </a>
-                                </p>
-
-                            </div>
-
+                        @foreach ( $products as $product)
+                            @include('tienda_cosmica.Components.item_categorias')
                         @endforeach
 
                     </div>
@@ -99,96 +59,16 @@
 
                 <div class="tab-pane fade" id="pills-facial" role="tabpanel" aria-labelledby="pills-facial-tab" tabindex="0">
                     <div class="row">
-                        @foreach ( $products_facial as $item)
-
-                            @php
-                                $nombre = $item->nombre;
-                                $partes = explode(' ', $nombre, 2); // Divide el nombre en máximo 2 partes
-                                $primeraPalabra = $partes[0]; // La primera palabra
-                                $restoDelTexto = $partes[1] ?? ''; // El resto del texto, si existe
-                            @endphp
-
-                            <div class="col-6 col-sm-6 col-md-4 col-lg-3">
-                                <div class="container_lineas_single_carousel mx-auto" style="width:80%; ">
-                                    <div class="content mb-3 mt-3">
-                                        <div class="img_container_single_corousel mx-auto">
-                                            <img class="img_grid_products_single_carousel" src="{{$item->imagenes}}" alt="Protector">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <h2 class="text-center titulo_producto_carousel mt-4">
-                                    <strong>{{ $primeraPalabra }}</strong> <br>
-                                    {{ $restoDelTexto }}
-                                </h2>
-
-                                <h4 class="text-center price_producto_carousel mt-3">
-                                    <strong class="">${{ $item->precio_normal }}.00</strong>
-                                </h4>
-
-                                <p class="text-center">
-
-                                    <a href="" class="text-center mt-3">
-                                        <img class="icon_single_item_slide" src="{{ asset('cosmika/inicio/CORAZON-POPULARES.png') }}" alt="" style="width: 25px;margin-right: auto;margin-left: auto;">
-                                    </a>
-                                </p>
-                                <p class="text-center">
-
-                                    <a href="" class="text-center mt-3">
-                                        <img class="icon_single_item_slide" src="{{ asset('cosmika/inicio/AGREGAR-POPULARES.png') }}" alt="" style="width: 25px;margin-right: auto;margin-left: auto;">
-                                    </a>
-                                </p>
-
-                            </div>
-
+                        @foreach ( $products_facial as $product)
+                            @include('tienda_cosmica.Components.item_categorias')
                         @endforeach
                     </div>
                 </div>
 
                 <div class="tab-pane fade" id="pills-corporal" role="tabpanel" aria-labelledby="pills-corporal-tab" tabindex="0">
                     <div class="row">
-                        @foreach ( $products_corporal as $item)
-
-                            @php
-                                $nombre = $item->nombre;
-                                $partes = explode(' ', $nombre, 2); // Divide el nombre en máximo 2 partes
-                                $primeraPalabra = $partes[0]; // La primera palabra
-                                $restoDelTexto = $partes[1] ?? ''; // El resto del texto, si existe
-                            @endphp
-
-                            <div class="col-6 col-sm-6 col-md-4 col-lg-3">
-                                <div class="container_lineas_single_carousel mx-auto" style="width:80%; ">
-                                    <div class="content mb-3 mt-3">
-                                        <div class="img_container_single_corousel mx-auto">
-                                            <img class="img_grid_products_single_carousel" src="{{$item->imagenes}}" alt="Protector">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <h2 class="text-center titulo_producto_carousel mt-4">
-                                    <strong>{{ $primeraPalabra }}</strong> <br>
-                                    {{ $restoDelTexto }}
-                                </h2>
-
-                                <h4 class="text-center price_producto_carousel mt-3">
-                                    <strong class="">${{ $item->precio_normal }}.00</strong>
-                                </h4>
-
-                                <p class="text-center">
-
-                                    <a href="" class="text-center mt-3">
-                                        <img class="icon_single_item_slide" src="{{ asset('cosmika/inicio/CORAZON-POPULARES.png') }}" alt="" style="width: 25px;margin-right: auto;margin-left: auto;">
-                                    </a>
-                                </p>
-                                <p class="text-center">
-
-                                    <a href="" class="text-center mt-3">
-                                        <img class="icon_single_item_slide" src="{{ asset('cosmika/inicio/AGREGAR-POPULARES.png') }}" alt="" style="width: 25px;margin-right: auto;margin-left: auto;">
-                                    </a>
-                                </p>
-
-                            </div>
-
+                        @foreach ( $products_corporal as $product)
+                            @include('tienda_cosmica.Components.item_categorias')
                         @endforeach
                     </div>
                 </div>
