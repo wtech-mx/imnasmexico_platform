937fa2d909d8e920164e320e9d53366c1eb7361d
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2025-04-15 12:47:42 -0600

    Poniendo letras mas greandes

--- resources/views/tienda_cosmica/diploma.blade.php
+++ resources/views/tienda_cosmica/diploma.blade.php
@@ -8,36 +8,42 @@
     bg_single_product
 @endsection
 
-
 @section('css_custom')
     <link href="{{asset('assets/user/custom/ecommerce_single.css')}}" rel="stylesheet" />
+    <style>
+        .custom-container {
+            height: 70vh; /* Ocupa toda la altura de la ventana */
+            display: flex; /* Activa Flexbox */
+            justify-content: center; /* Centra horizontalmente */
+            align-items: center; /* Centra verticalmente */
+            margin: 0 auto; /* Centra el contenedor horizontalmente */
+        }
+    </style>
 @endsection
 
 @section('content')
 
-<div class="container ">
+<div class="container custom-container">
 
-    <div class="row">
-        <div class="col-3"></div>
-        <div class="col-6 p-3 p-sm-2 p-md-3 p-lg-3">
+    <div class="row justify-content-center">
+        <div class="col-12 p-4">
             <div class="mt-5 mb-5">
-                <h2 class="text-center">Generar Diploma</h2>
+                <h2 class="text-center mb-5">Generar Diploma</h2>
                 <form action="{{ route('diploma_tratamientos.generar') }}" method="POST">
                     @csrf
                     <div class="mb-3">
-                        <label for="nombre" class="form-label">Ingresa su Nombre Completo:</label>
+                        <label for="nombre" class="form-label" style="font-size: 26px">Ingrese su Nombre Completo:</label>
                         <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Escribe tu nombre completo" required>
                     </div>
-                    <button type="submit" class="btn btn-sm btn-success">Generar Diploma</button>
+                    <div class="text-center mt-5">
+                        <button type="submit" class="btn btn-sm btn-success" style="background: #2D2432">Generar Diploma</button>
+                    </div>
                 </form>
             </div>
         </div>
-        <div class="col-3"></div>
-
     </div>
 
 </div>
-
 
 @endsection
 
