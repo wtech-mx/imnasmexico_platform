75d99971973e5c96aa4b07d2067270c1a5bc133e
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2025-04-24 15:11:14 -0600

    incio ruta

--- resources/views/cotizador/index_cosmica.blade.php
+++ resources/views/cotizador/index_cosmica.blade.php
@@ -42,7 +42,7 @@ Cosmica
                                     <div class="item">
                                         <div class="product_category" onclick="cargarProductosPorCategoria('{{ $categoria->nombre }}')">
                                             <h6 class="mt-3 mb-1 tittle_category">Corporal</h6>
-                                            <img src="{{ asset('cosmika\INICIO\lineas/'.$categoria->nombre.'.png') }}" alt="Producto">
+                                            <img src="{{ asset('cosmika\lineas\lineas/'.$categoria->nombre.'.png') }}" alt="Producto">
                                             <h6 class="mt-3 mb-1 tittle_category">{{ $categoria->nombre }}</h6>
                                             <div class="fw-bold mt-1">
                                                 <p class="text_items" style="margin: 0;">
