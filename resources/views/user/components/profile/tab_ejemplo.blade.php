ffb06f28f692b2edfea1d0bc3a75ef92de44ee45
verplancken <42614785+verplancken@users.noreply.github.com>
2025-04-30 12:18:39 -0600

    textos en perfil

--- resources/views/user/components/profile/tab_ejemplo.blade.php
+++ resources/views/user/components/profile/tab_ejemplo.blade.php
@@ -1,6 +1,19 @@
 <div class="modal-body row">
     <div class="row">
             <div class="col-6  mb-2">
+                <a href="{{asset('documentos/FICHA CLINICA FACIAL.pdf') }}" download="FICHA CLINICA FACIAL" style="text-decoration: none; color: #000">
+                    <img src="{{asset('assets/user/icons/pdf.png') }}" style="width: 45px; height: 45px;"/>
+                    FICHA CLINICA FACIAL
+                </a>
+            </div>
+            <div class="col-6  mb-2">
+                <iframe class="mt-2" src="{{asset('documentos/FICHA CLINICA FACIAL.pdf')}}" style="width: 60%; height: 60px;"></iframe>
+                <p class="text-center ">
+                    <a class="btn btn-sm text-dark" href="{{asset('documentos/FICHA CLINICA FACIAL.pdf') }}" target="_blank" style="background: #836262; color: #ffff!important">Ver archivo</a>
+                </p>
+            </div>
+
+            <div class="col-6  mb-2">
                 <a href="{{asset('documentos/FICHA CLINICA CORPORAL.pdf') }}" download="FICHA CLINICA CORPORAL" style="text-decoration: none; color: #000">
                     <img src="{{asset('assets/user/icons/pdf.png') }}" style="width: 45px; height: 45px;"/>
                     FICHA CLINICA CORPORAL
@@ -23,19 +36,6 @@
                 <iframe class="mt-2" src="{{asset('documentos/EJEMPLO PORTAFOLIO EC0010.pdf')}}" style="width: 60%; height: 60px;"></iframe>
                 <p class="text-center ">
                     <a class="btn btn-sm text-dark" href="{{asset('documentos/EJEMPLO PORTAFOLIO EC0010.pdf') }}" target="_blank" style="background: #836262; color: #ffff!important">Ver archivo</a>
-                </p>
-            </div>
-
-            <div class="col-6  mb-2">
-                <a href="{{asset('documentos/FICHA CLINICA FACIAL.pdf') }}" download="FICHA CLINICA FACIAL" style="text-decoration: none; color: #000">
-                    <img src="{{asset('assets/user/icons/pdf.png') }}" style="width: 45px; height: 45px;"/>
-                    FICHA CLINICA FACIAL
-                </a>
-            </div>
-            <div class="col-6  mb-2">
-                <iframe class="mt-2" src="{{asset('documentos/FICHA CLINICA FACIAL.pdf')}}" style="width: 60%; height: 60px;"></iframe>
-                <p class="text-center ">
-                    <a class="btn btn-sm text-dark" href="{{asset('documentos/FICHA CLINICA FACIAL.pdf') }}" target="_blank" style="background: #836262; color: #ffff!important">Ver archivo</a>
                 </p>
             </div>
 
