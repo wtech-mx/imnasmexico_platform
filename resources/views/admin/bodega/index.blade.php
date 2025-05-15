85579c49f2231928d03656617b8501e6161211c7
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2025-05-15 11:57:04 -0600

    Actualzacion de btn

--- resources/views/admin/bodega/index.blade.php
+++ resources/views/admin/bodega/index.blade.php
@@ -183,6 +183,10 @@
                                                     </td>
                                                     <td><h5>${{ $item->total }}</h5></td>
                                                     <td>
+                                                        <a class="btn btn-xs btn-warning text-white" target="_blank" href="{{ route('notas_productos.imprimir', ['id' => $item->id]) }}">
+                                                            <i class="fa fa-file"></i>
+                                                        </a>
+
                                                         <a class="btn btn-sm btn-info text-white" target="_blank" href="{{ route('notas_cotizacion.imprimir', ['id' => $item->id]) }}">
                                                             <i class="fa fa-list-alt"></i>
                                                         </a>
