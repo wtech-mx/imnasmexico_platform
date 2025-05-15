364ad601ac36454e0f6ab9e9593e5c962ec6191a
verplancken <42614785+verplancken@users.noreply.github.com>
2025-05-15 13:01:59 -0600

    nas repo

--- app/Http/Controllers/PerfilClienteController.php
+++ app/Http/Controllers/PerfilClienteController.php
@@ -395,7 +395,7 @@ class PerfilClienteController extends Controller
 
     public function getProductosByNota($notaId)
     {
-        $productos = ProductosNotasCosmica::where('id_notas_productos', $notaId)->get();
+        $productos = ProductosNotasId::where('id_notas_productos', $notaId)->get();
         return response()->json($productos);
     }
 
