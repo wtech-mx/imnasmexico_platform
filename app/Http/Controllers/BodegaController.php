db51f3c8d3d437a51ad9aa5736b2e59016d20416
verplancken <42614785+verplancken@users.noreply.github.com>
2025-05-18 21:50:33 -0600

    Update BodegaController.php

--- app/Http/Controllers/BodegaController.php
+++ app/Http/Controllers/BodegaController.php
@@ -267,30 +267,30 @@ class BodegaController extends Controller
         $primerDiaDelMes = date('Y-m-01');
         $ultimoDiaDelMes = date('Y-m-t');
         // Crear instancia del cliente Automattic\WooCommerce\Client para la tienda principal
-        // $woocommerce = new Client(
-        //     'https://imnasmexico.com/new/', // URL de la tienda principal
-        //     'ck_9e19b038c973d3fdf0dcafe8c0352c78a16cad3f', // Consumer Key de la tienda principal
-        //     'cs_762a289843cea2a92751f757f351d3522147997b', // Consumer Secret de la tienda principal
-        //     [
-        //         'wp_api' => true,
-        //         'version' => 'wc/v3',
-        //     ]
-        // );
+        $woocommerce = new Client(
+            'https://imnasmexico.com/new/', // URL de la tienda principal
+            'ck_9e19b038c973d3fdf0dcafe8c0352c78a16cad3f', // Consumer Key de la tienda principal
+            'cs_762a289843cea2a92751f757f351d3522147997b', // Consumer Secret de la tienda principal
+            [
+                'wp_api' => true,
+                'version' => 'wc/v3',
+            ]
+        );
 
 
-        // // Obtener los pedidos de ambas tiendas con el estado "guia_cargada"
-        // $orders_tienda_principal = $woocommerce->get('orders', [
-        //     'status' => 'guia_cargada',
-        //     'per_page' => 100,
-        // ]);
+        // Obtener los pedidos de ambas tiendas con el estado "guia_cargada"
+        $orders_tienda_principal = $woocommerce->get('orders', [
+            'status' => 'guia_cargada',
+            'per_page' => 100,
+        ]);
 
-        // $orders_tienda_principal_preparados = $woocommerce->get('orders', [
-        //     'status' => 'preparados',
-        //     'per_page' => 100,
-        // ]);
+        $orders_tienda_principal_preparados = $woocommerce->get('orders', [
+            'status' => 'preparados',
+            'per_page' => 100,
+        ]);
 
-        // // Convertir las órdenes en un solo array combinando las de ambas tiendas
-        // $orders_tienda_principal = array_merge($orders_tienda_principal);
+        // Convertir las órdenes en un solo array combinando las de ambas tiendas
+        $orders_tienda_principal = array_merge($orders_tienda_principal);
         // dd($orders_tienda_principal);
 
         $dominio = $request->getHost();
