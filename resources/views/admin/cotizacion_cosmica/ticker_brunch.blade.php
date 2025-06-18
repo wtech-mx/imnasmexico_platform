7a941d8ebcabcacba44f9ef54ffd592c2b3d127f
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2025-06-18 09:36:02 -0600

    Ticket brunch estatico

--- resources/views/admin/cotizacion_cosmica/ticker_brunch.blade.php
+++ resources/views/admin/cotizacion_cosmica/ticker_brunch.blade.php
@@ -1,102 +1,61 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<!DOCTYPE html>
+<html lang="en" >
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-  <link rel="stylesheet" href="https://zocofresh.com/assets/css/ticket.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+  <meta charset="UTF-8">
+  <title>Ticket Brunch </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://public.codepenassets.com/css/reset-2.0.min.css">
+    <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 
   <style>
-    body {
-        background-color: #414141!important;
-    }
+        @import url("https://fonts.googleapis.com/css2?family=Staatliches&display=swap");
+        @import url("https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap");
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body,
+        html {
+            height: 100vh;
+            display: grid;
+            font-family: "Staatliches", cursive;
+            background: #fff;
+            color: black;
+            font-size: 14px;
+            letter-spacing: 0.1em;
+        }
+
+        .ticket {
+            margin: auto;
+            display: flex;
+            background: white;
+            box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
+        }
+
+        .nombre{
+            position: absolute;
+            color:#fff;
+            left:670px;
+            top: 125px;
+            line-height: 30px
+        }
+
+        img{
+            width: 900px;
+        }
+
   </style>
+
 </head>
-<body>
+    <body>
 
-<main class="ticket-system">
-   <div class="top">
-   <h1 class="title">Espera un segundo, tu tiket se está imprimiendo.</h1>
-   <div class="printer">
-   </div>
-
-   <div class="receipts-wrapper">
-        <div class="receipt">
-<div class="receipt">
-            <div class="details">
-                <div class="item"> </div>
-                <div class="item">
-                    <img class="logo_ticket" src="{{ asset('ecommerce/Logo_horizontal_negro.png') }}" style="width: 180px !important;">
-                </div>
-                <div class="item"></div>
-            </div>
-
-            <div class="d-flex justify-content-center">
-                <p class="text-center" style="font-size: 10px">
-                    ZOCOFRESH <br>
-                    Tel:558914-2564 / 5589142565 <br>
-                    RFC:ZFR2306262N7 <br>
-                    ventas@zocofresh.com.mx / zocofresh@gmail.com <br>
-                    zocofresh.com <br>
-                    ZOCO FREESH IGNACIO RAYON 51 <br>
-                    BARRIO SAN FELIPE <br>
-                    51950 - TONATICO <br>
-                    México <br>
-                    SUCURSAL MÉRIDA 64, ROMA NORTE, CUAUHTEMOC , <br>
-                    06700, CDMX <br>
-                    ------------------- <br>
-                    Servido por "" <br>
-                    <strong style="font-size: 19px;"></strong>
-                </p>
-            </div>
-
-
-            <div class="details row">
-
-                {{-- Datos de venta --}}
-
-                <p class="text-center" style="font-size: 10px">
-                    ZOCOFRESH <br>
-                    Tel:558914-2564 / 5589142565 <br>
-                    RFC:ZFR2306262N7 <br>
-                    ventas@zocofresh.com.mx / zocofresh@gmail.com <br>
-                    zocofresh.com <br>
-                    ZOCO FREESH IGNACIO RAYON 51 <br>
-                    BARRIO SAN FELIPE <br>
-                    51950 - TONATICO <br>
-                    México <br>
-                    SUCURSAL MÉRIDA 64, ROMA NORTE, CUAUHTEMOC , <br>
-                    06700, CDMX <br>
-                    ------------------- <br>
-                    Servido por "" <br>
-                    <strong style="font-size: 19px;"></strong>
-                </p>
-
-
-            </div>
+        <div class="ticket" style="position: relative">
+            <img class="img" src="{{ asset('cosmika/elementos/entrada.png') }}" alt="">
+            <p class="nombre">Juana de arco de alarcon Vega lopez mega allende</p>
         </div>
 
-        <div class="receipt qr-code">
-
-            {{-- qr dinamico --}}
-
-
-            <div class="description">
-            <h2 style="font-size: 21px;">¡Gracias por su Compra! </h2>
-            </div>
-        </div>
-        </div>
-   </div>
-
-</main>
-
-</body>
-
+    </body>
 </html>
