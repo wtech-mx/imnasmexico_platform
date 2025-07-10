3a03ddbc8db008cdb613d3a3286e8ac7f0afc7f6
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2025-07-10 11:52:11 -0600

    Actualzaiuconde agrgando bicador

---
+++ resources/views/cotizador/compnent_buscador_productos.blade.php
@@ -0,0 +1,11 @@
+                <div class="col-12">
+                    <form class="d-flex mt-3 mb-3" id="formBuscarProductos">
+                        <input id="inputBuscarProductos" class="form-control me-2" type="search" placeholder="Buscar producto..." aria-label="Search">
+                    </form>
+                </div>
+
+                <!-- Repetir este div para cada producto -->
+                    <div class="" id="contenedor_productos">
+                        <!-- Aquí se insertan los productos dinámicamente -->
+                    </div>
+                <!-- ... más productos -->
